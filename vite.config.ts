2dd71daae808c8d0316592971d08807db810c3dc
Hetari <hetari4py@gmail.com>
2024-08-28 22:22:10 +0300

    try to fix the auto route

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueRouter from 'unplugin-vue-router/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueRouter from 'unplugin-vue-router/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/100/',
   plugins: [
     VueRouter({
       /* options */
     }),
     // ⚠️ Vue must be placed after VueRouter()
-    vue(),
-  ],
-})
+    vue()
+  ]
+});

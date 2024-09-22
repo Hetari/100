b8bc4c90479524a3785de74d853695821db4f3fb
Hetari <hetari4py@gmail.com>
2024-09-22 22:09:29 +0300

    Basic Star Movement

--- src/pages/100/1.vue
+++ src/pages/100/1.vue
@@ -7,27 +7,46 @@
 <script setup lang="ts">
   import P5 from 'p5';
   import { onMounted, ref } from 'vue';
+  import { get, useElementHover } from '@vueuse/core';
 
   const canvas = ref<HTMLDivElement | null>();
   const numStars = 800;
+  const speed = ref(5);
 
   class Star {
     p: P5;
     x: number;
     y: number;
+    z: number;
 
     constructor(p: P5) {
       this.p = p;
       this.x = p.random(-p.width, p.width);
       this.y = p.random(-p.height, p.height);
+      this.z = p.random(p.width); // Depth of the star
+    }
+
+    update() {
+      this.z -= get(speed);
+
+      if (this.z < 1) {
+        // Reset if it's too close
+        this.z = this.p.width;
+        this.x = this.p.random(-this.p.width, this.p.width);
+        this.y = this.p.random(-this.p.height, this.p.height);
+      }
     }
 
     show() {
       this.p.fill(255);
       this.p.noStroke();
 
+      // Perspective scaling
+      const sx = this.p.map(this.x / this.z, 0, 1, 0, this.p.width);
+      const sy = this.p.map(this.y / this.z, 0, 1, 0, this.p.height);
+
       // Draw a simple circle for the star
-      this.p.ellipse(this.x, this.y, 8, 8);
+      this.p.ellipse(sx, sy, 8, 8);
     }
   }
 
@@ -49,6 +68,7 @@
           sketch.translate(sketch.width / 2, sketch.height / 2);
 
           stars.forEach((star) => {
+            star.update();
             star.show();
           });
         };

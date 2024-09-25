d2b8cb647cd7fc1a7d2483fa03680d5f2e80b928
Hetari <hetari4py@gmail.com>
2024-09-25 14:49:50 +0300

    add 404 page

---
+++ src/pages/[...path].vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex-center h-screen text-xl sm:text-7xl lg:text-9xl">
+    404 - Not found
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
